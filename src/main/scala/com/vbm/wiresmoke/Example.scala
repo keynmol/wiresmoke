01670cc198d9639633ce3ecf7cb5be0b7ab0ca95
keynmol <keynmol@gmail.com>
2019-11-30 16:54:39 +0000

    Cleanup example

--- src/main/scala/com/vbm/wiresmoke/Example.scala
+++ src/main/scala/com/vbm/wiresmoke/Example.scala
@@ -1,11 +1,8 @@
 package com.vbm.wiresmoke
+
 import cats.effect._
-import cats.data.Kleisli
 import org.http4s.Uri
-import org.http4s.EntityEncoder
 import org.http4s.client.Client
-import cats.effect.concurrent.Ref
-import org.http4s.EntityDecoder
 import scala.concurrent.ExecutionContext.global
 
 import org.http4s.client.blaze._
@@ -14,30 +11,16 @@ import org.http4s.Response
 
 import cats.syntax.all._
 import org.http4s.client.dsl.Http4sClientDsl
-import org.http4s.HttpRoutes
-import io.circe.{Encoder, Decoder, Json, HCursor}
-import io.circe.generic.semiauto._
 
-import org.http4s.circe._
-import cats.Applicative
-import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.server.middleware.Logger
-import org.http4s.Method
-import org.http4s.Request
-import org.http4s.Header
-import org.http4s.util.CaseInsensitiveString
 
-object Example {
+object Example extends Http4sClientDsl[IO] with Http4sDsl[IO] {
   import Wiresmoke._  
 
   def main(args: Array[String]): Unit = {
-    val dsl = new Http4sDsl[IO] {}
-    import dsl._
-
     implicit val t: Timer[IO]         = IO.timer(global)
     implicit val cs: ContextShift[IO] = IO.contextShift(global)
 
-    case class MyService(client: Client[IO]) extends Http4sClientDsl[IO] with Http4sDsl[IO] {
+    case class MyService(client: Client[IO])  {
       def call(num: Int): IO[String] = {
         val uri = Uri.unsafeFromString(s"http://oneid.com/hello/${num}")
 
